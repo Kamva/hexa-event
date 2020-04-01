4d8f58713f009121f593d02d1f5acf64bbefb255
Mehran Prs <mehr.prs@gmail.com>
2020-04-02 02:14:55 +0430

    Add helper methods

--- pulsar/util.go
+++ pulsar/util.go
@@ -5,6 +5,8 @@ import (
 )
 
 type (
+	SubscribeItemPackList []SubscribeItemPack
+
 	// SubscribeItemPack is a helper struct to help you to have both consumer options and subscription Item in single struct.
 	// See its example in the "examples" directory.
 	SubscribeItemPack struct {
@@ -12,6 +14,24 @@ type (
 		SubscriptionItem hevent.SubscriptionItem
 	}
 )
+
+// ConsumerOptions returns list of Consumer options.
+func (l SubscribeItemPackList) ConsumerOptions() []ConsumerOptionsItem {
+	list := make([]ConsumerOptionsItem, len(l))
+	for i, v := range l {
+		list[i] = v.ConsumerOptions
+	}
+	return list
+}
+
+// SubscriptionItems returns list of SubscriptionItems
+func (l SubscribeItemPackList) SubscriptionItems() []hevent.SubscriptionItem {
+	list := make([]hevent.SubscriptionItem, len(l))
+	for i, v := range l {
+		list[i] = v.SubscriptionItem
+	}
+	return list
+}
 
 // DefaultSubscriptionItemPack returns new instance of the SubscribeItemPack
 func DefaultSubscriptionItemPack(channel string, payloadInstance interface{}, handler hevent.EventHandler) SubscribeItemPack {
