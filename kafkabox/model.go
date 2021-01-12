12f655747320a97af5b6f53bab8cbd1585268f31
mehran-prs <mehr.prs@gmail.com>
2021-01-12 11:09:30 +0330

    Add bson fields

--- kafkabox/model.go
+++ kafkabox/model.go
@@ -10,10 +10,10 @@ type Header struct {
 
 // OutboxMessage is the outbox collection's model
 type OutboxMessage struct {
-	ID        string    `json:"id"`
-	Topic     string    `json:"topic"`
-	Key       string    `json:"key"`
-	Value     string    `json:"value"`
-	Headers   []Header  `json:"headers"`
-	EmittedAt time.Time `json:"emitted_at"`
+	ID        string    `bson:"id" json:"id"`
+	Topic     string    `bson:"topic" json:"topic"`
+	Key       string    `bson:"key" json:"key"`
+	Value     string    `bson:"value" json:"value"`
+	Headers   []Header  `bson:"headers" json:"headers"`
+	EmittedAt time.Time `bson:"emitted_at" json:"emitted_at"`
 }
